9d86ca9ecfe7aaddf9ddd4ff4f0fd72393427c8c
AltheaIX <althea.ixy@gmail.com>
2025-06-10 01:29:57 +0700

    fix: structure

---
+++ src/UMM Library Backend/src/main/java/com/example/umm_library_backend/UmmLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.umm_library_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmmLibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmmLibraryBackendApplication.class, args);
+    }
+
+}
